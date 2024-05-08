ee9977bfedc86ca87abc9a7b260a5b8830961fa7
Justin Schow <justin.schow@gmail.com>
2024-05-08 09:05:35 -0600

    Add seedling.png image to project, update landing page

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 144, 251, 162), // Set background color here
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+              child: Image.asset(
+                'assets/seedling.png', // Path to the image file
+                width: 100, // Adjust the width as needed
+                height: 100, // Adjust the height as needed
+              ),
+            ),
+            SizedBox(height: 20), // Adding some space between the image and text
+            Text(
+              'Seedling',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'A Language Learning App',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
