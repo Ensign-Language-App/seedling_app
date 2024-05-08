52218b824a928f87ecfeaeb3b692288cca84ff78
Justin Schow <justin.schow@gmail.com>
2024-05-08 10:53:35 -0600

    Update background

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 137, 248, 156), // Set background color here
+      backgroundColor: Color.fromARGB(255, 126, 246, 146), // Set background color here
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
