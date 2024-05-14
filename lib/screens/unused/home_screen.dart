6ffaa3e21998de0f383c48120aeaa116edfefce5
JNope <91224053+JNope01@users.noreply.github.com>
2024-05-14 15:41:39 -0600

    ran a couple auto fixes

--- lib/screens/unused/home_screen.dart
+++ lib/screens/unused/home_screen.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:
-          Color.fromARGB(255, 115, 250, 138), // Set background color here
+          const Color.fromARGB(255, 115, 250, 138), // Set background color here
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ColorFiltered(
-              colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+              colorFilter: const ColorFilter.mode(Colors.black, BlendMode.srcIn),
               child: Image.asset(
                 'assets/seedling.png', // Path to the image file
                 width: 100, // Adjust the width as needed
                 height: 100, // Adjust the height as needed
               ),
             ),
-            SizedBox(
+            const SizedBox(
                 height: 20), // Adding some space between the image and text
-            Text(
+            const Text(
               'Seedling',
               style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
             ),
-            Text(
+            const Text(
               'The Language Learning App',
               style: TextStyle(fontSize: 24),
             ),
