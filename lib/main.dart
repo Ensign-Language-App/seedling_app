5b9b9f57f6d0ea308a8c4c158802ce6b5a98b4ac
franklee <4anklee@gmail.com>
2024-05-13 23:24:25 -0600

    created a home page, and some modification of theme color. May 13, 2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:seedling_app/screens/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
