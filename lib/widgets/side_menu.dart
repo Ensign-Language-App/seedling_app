f66358acd8dd772001f3ee4f85b7eb9d653b90ef
LSG2024 <lsg486@ensign.edu>
2024-06-04 23:18:00 -0600

    Merge remote-tracking branch 'origin/main'

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:seedling_app/screens/log_in_page.dart';
 import 'package:seedling_app/providers/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:seedling_app/screens/settings_page.dart'; // Importing the SettingsView widget file
 
 class SideMenu extends StatelessWidget {
-  const SideMenu({super.key});
+  const SideMenu({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,37 +17,39 @@ class SideMenu extends StatelessWidget {
               color: Colors.lightGreen,
             ),
             child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: NetworkImage(
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2eBwWNVsx4cmRUBRM9jip0fzz4rej8CmQ-Q&usqp=CAU'),
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2eBwWNVsx4cmRUBRM9jip0fzz4rej8CmQ-Q&usqp=CAU'),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Duo',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Duo',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                ]),
+                ),
+              ],
+            ),
           ),
           const ListTile(
             leading: Icon(Icons.person),
             title: Text('Profile'),
             onTap: null,
           ),
-          const ListTile(
-            leading: Icon(Icons.info),
-            title: Text('About'),
-            onTap: null,
-          ),
-          const ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: null,
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () {
+              Navigator.pop(context); // Close the drawer
+              Navigator.push( // Navigate to the SettingsView
+                context,
+                MaterialPageRoute(builder: (context) => SettingsView(showMenu: ValueNotifier<bool>(false))),
+              );
+            },
           ),
           ListTile(
             leading: const Icon(Icons.dark_mode),
@@ -59,8 +62,8 @@ class SideMenu extends StatelessWidget {
             ),
           ),
           const Padding(
-            padding: EdgeInsets.only(top: 300),
-            child: Divider()
+              padding: EdgeInsets.only(top: 300),
+              child: Divider()
           ),
           ListTile(
             leading: const Icon(Icons.logout),
