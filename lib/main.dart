700a6ea534cad0a1c62902f048963f1dac1635fd
lsingerkira <lsingerafonso@gmail.com>
2024-06-10 14:55:24 -0600

    nav to learning page

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,7 @@ void main() => runApp(
   ),
 );
 
-<<<<<<< HEAD
-class MyApp extends StatelessWidget{
-=======
 class MyApp extends StatelessWidget {
->>>>>>> 20ca6cb81bbacd458b1efca712b824d03a1d16e7
   const MyApp({super.key});
 
   @override
@@ -30,7 +26,6 @@ class MyApp extends StatelessWidget {
     final themeNotifier = Provider.of<ThemeNotifier>(context);
 
     return MaterialApp(
-<<<<<<< HEAD
       theme: themeNotifier.isDarkMode ? darkMode : lightMode,
       darkTheme: darkMode,
       debugShowCheckedModeBanner: false,
@@ -39,13 +34,6 @@ class MyApp extends StatelessWidget {
         '/': (context) => const HomePage(),
         '/learn': (context) => const LanguageLearningApp(),
       },
-=======
-      theme: lightMode,
-      darkTheme: darkMode,
-      themeMode: themeNotifier.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      home: const LandingScreen(),
->>>>>>> 20ca6cb81bbacd458b1efca712b824d03a1d16e7
     );
   }
 }
