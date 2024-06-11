b773647f24970af1b8a93de7c4567fabd942462e
Frank Lee <144109238+4anklee@users.noreply.github.com>
2024-06-11 17:40:33 -0600

    fix some other navigation errors between pages, Jun 11, 2024

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:seedling_app/screens/home_page.dart';
+import 'package:seedling_app/screens/learn_page.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
+
+  @override
+  BottomNavBarState createState() => BottomNavBarState();
+
+}
+
+class BottomNavBarState extends State<BottomNavBar> {
+  int activeIndex = 0;
+
+  List<Widget> pages = const [
+    HomePage(),
+    LearningPage(),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: activeIndex,
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
           icon: Icon(Icons.home),
@@ -20,6 +37,23 @@ class BottomNavBar extends StatelessWidget {
           label: 'Bookmark',
         ),
       ],
+      onTap: (int index) {
+        if (index != activeIndex) {
+          setState(() {
+            activeIndex = index;
+          });
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => pages[activeIndex],
+            ),
+          ).then((_) {
+            setState(() {
+              activeIndex = 0;
+            });
+          });
+        }
+      },
     );
   }
 }
