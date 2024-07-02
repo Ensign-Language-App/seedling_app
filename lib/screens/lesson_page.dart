9fecead890a76fdb81e24552cd7308f513f58610
Justin Schow <justin.schow@gmail.com>
2024-07-02 07:33:00 -0600

    Update lesson page with firestore test (*not working yet*)

--- lib/screens/lesson_page.dart
+++ lib/screens/lesson_page.dart
@@ -106,66 +106,92 @@ class LessonPageState extends State<LessonPage> {
 }
 
 
+
 // import 'package:flutter/material.dart';
-// import 'package:flip_card/flip_card.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'dart:async';
 
-// void main() => runApp(
-//       const MaterialApp(
-//         home: LessonPage(),
-//       ),
-//     );
-
-// class LessonPage extends StatelessWidget {
+// class LessonPage extends StatefulWidget {
 //   const LessonPage({super.key});
+
+//   @override
+//   LessonPageState createState() => LessonPageState();
+// }
+
+// class LessonPageState extends State<LessonPage> {
+//   bool isLoading = true;
+//   String message = "Loading...";
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchSingleWordWithRetry();
+//   }
+
+//   Future<void> fetchSingleWordWithRetry() async {
+//     const maxRetries = 5;
+//     const backoffDuration = Duration(seconds: 2);
+//     int retryCount = 0;
+
+//     while (retryCount < maxRetries) {
+//       try {
+//         DocumentSnapshot doc = await FirebaseFirestore.instance
+//             .collection('Subjects')
+//             .doc('Test')
+//             .collection('Words')
+//             .doc('Dog')
+//             .get();
+
+//         if (doc.exists) {
+//           print('Fetched document: ${doc.id} with data: ${doc.data()}');
+//           setState(() {
+//             message = 'Fetched document: ${doc.id} with data: ${doc.data()}';
+//             isLoading = false;
+//           });
+//           return;
+//         } else {
+//           print('Document does not exist');
+//           setState(() {
+//             message = 'Document does not exist';
+//             isLoading = false;
+//           });
+//           return;
+//         }
+//       } catch (e) {
+//         print('Error fetching word: $e');
+//         retryCount++;
+//         if (retryCount < maxRetries) {
+//           await Future.delayed(backoffDuration * retryCount);
+//         } else {
+//           setState(() {
+//             message = 'Error fetching word: $e';
+//             isLoading = false;
+//           });
+//           return;
+//         }
+//       }
+//     }
+//   }
 
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//       backgroundColor: const Color.fromRGBO(255, 150, 79, 1.0),  // Using RGB values
-//       // backgroundColor: Colors.lightGreen, // Using predefined color
-//       appBar: AppBar(title: const Text('Learn')),
+//       appBar: AppBar(title: const Text('Firestore Test')),
 //       body: Center(
-//         child: FlipCard(
-//           direction: FlipDirection.HORIZONTAL,
-//           front: Card(
-//             elevation: 8.0,
-//             shape: RoundedRectangleBorder(
-//               borderRadius: BorderRadius.circular(10.0),
-//             ),
-//             child: Container(
-//               height: 200,
-//               width: 300,
-//               alignment: Alignment.center,
-//               child: const Text(
-//                 'The best anime \never made is...',
-//                 style: TextStyle(
-//                   fontSize: 24.0,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ),
-//           ),
-//           back: Card(
-//             elevation: 8.0,
-//             shape: RoundedRectangleBorder(
-//               borderRadius: BorderRadius.circular(10.0),
-//             ),
-//             child: Container(
-//               height: 200,
-//               width: 300,
-//               alignment: Alignment.center,
-//               child: const Text(
-//                 'Saint Seiya',
-//                 style: TextStyle(
-//                   fontSize: 24.0,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
+//         child: isLoading
+//             ? CircularProgressIndicator()
+//             : Text(message),
 //       ),
 //     );
 //   }
 // }
+
+
+
+
+
+
+
+
 
