9a87d54508ba53d34dc5b3214e57e90a8225a3e7
Justin Schow <justin.schow@gmail.com>
2024-07-10 12:55:15 -0600

    Prepare upload_vocab.py for firestore upload

--- scripts/upload_vocab.py
+++ scripts/upload_vocab.py
@@ -18,12 +18,13 @@ with open('path/to/your/vocab.json', 'r', encoding='utf-8') as f:
 
 # Function to upload vocabulary data to Firestore
 def upload_vocab_to_firestore(data, collection_name='vocabulary'):
-    for subject, subject_data in data['Subjects'].items():
-        for topic, topic_data in subject_data.items():
-            for word, translations in topic_data.items():
-                doc_ref = db.collection(collection_name).document(subject).collection(topic).document(word)
-                doc_ref.set(translations)
-                print(f'Uploaded {word} in {subject}/{topic}')
+    for vocabulary, vocab_data in data.items():
+        for subject, subject_data in vocab_data.items():
+            for topic, topic_data in subject_data.items():
+                for word, translations in topic_data.items():
+                    doc_ref = db.collection(collection_name).document(subject).collection(topic).document(word)
+                    doc_ref.set(translations)
+                    print(f'Uploaded {word} in {subject}/{topic}')
 
 # Upload the vocabulary data
 upload_vocab_to_firestore(vocab_data)
