db42bac0662bd74e39e09bf310294c96801366ab
Justin Schow <justin.schow@gmail.com>
2024-07-15 08:56:28 -0600

    Add subjects to home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -312,7 +312,217 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ],
-            ),            
+            ),
+                        SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/clothing.png',
+                    title: 'Colors',
+                    lessonColor: Color(0xFFf6bd60),
+                    stackColor1: Color(0xFFf5cac3),
+                    stackColor2: Color(0xFFf7ede2),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Colors',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/places.png',
+                    title: 'Days',
+                    lessonColor: Color(0xFFe9edc9),
+                    stackColor1: Color(0xFFfaedcd),
+                    stackColor2: Color(0xFFfefae0),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Days',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/the_body.png',
+                    title: 'Months',
+                    lessonColor: Color(0xFFff8fab),
+                    stackColor1: Color(0xFFffb3c6),
+                    stackColor2: Color(0xFFffe5ec),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Months',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/traffics.png',
+                    title: 'Time',
+                    lessonColor: Color(0xFFff9f1c),
+                    stackColor1: Color(0xFFffbf69),
+                    stackColor2: Color(0xFFffffff),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Time',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/animals.png',
+                    title: 'Questions',
+                    lessonColor: Color(0xFF9c6644),
+                    stackColor1: Color(0xFF7f5539),
+                    stackColor2: Color(0xFFe6ccb2),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Questions',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/greets.png',
+                    title: 'Conversation',
+                    lessonColor: Color(0xFF8d99ae),
+                    stackColor1: Color(0xFF2b2d42),
+                    stackColor2: Color(0xFFedf2f4),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Conversation',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/family.png',
+                    title: 'Adjectives',
+                    lessonColor: Color(0xFF83c5be),
+                    stackColor1: Color(0xFF006d77),
+                    stackColor2: Color(0xFFb08968),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Adjectives',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/the_body.png',
+                    title: 'Verbs',
+                    lessonColor: Color(0xFFff8fab),
+                    stackColor1: Color(0xFFffb3c6),
+                    stackColor2: Color(0xFFffe5ec),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Verbs',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/numbers.png',
+                    title: 'Regular Verbs',
+                    lessonColor: Color(0xFF48cae4),
+                    stackColor1: Color(0xFFade8f4),
+                    stackColor2: Color(0xFFcaf0f8),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Regular Verbs',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/food.png',
+                    title: 'Irregular Verbs',
+                    lessonColor: Color(0xFFd5bdaf),
+                    stackColor1: Color(0xFFe3d5ca),
+                    stackColor2: Color(0xFFf5ebe0),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Irregular Verbs',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/clothing.png',
+                    title: 'Subject Pronouns',
+                    lessonColor: Color(0xFFf6bd60),
+                    stackColor1: Color(0xFFf5cac3),
+                    stackColor2: Color(0xFFf7ede2),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Subject Pronouns',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/places.png',
+                    title: 'Possessive Pronouns',
+                    lessonColor: Color(0xFFe9edc9),
+                    stackColor1: Color(0xFFfaedcd),
+                    stackColor2: Color(0xFFfefae0),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Possessive Pronouns',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/the_body.png',
+                    title: 'Prepositions',
+                    lessonColor: Color(0xFFff8fab),
+                    stackColor1: Color(0xFFffb3c6),
+                    stackColor2: Color(0xFFffe5ec),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Prepositions',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/traffics.png',
+                    title: 'Conjunctions',
+                    lessonColor: Color(0xFFff9f1c),
+                    stackColor1: Color(0xFFffbf69),
+                    stackColor2: Color(0xFFffffff),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Conjunctions',
+                  ),
+                ),
+              ],
+            ),
             SizedBox(height: 40),
           ],
         ),
