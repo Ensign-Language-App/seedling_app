e0ed1a7b81bb42fd3e3c60daa6079f842e4843e8
Justin Schow <justin.schow@gmail.com>
2024-07-15 08:48:04 -0600

    Add subjects to home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -164,6 +164,156 @@ class HomePage extends StatelessWidget {
               ],
             ),
             SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/animals.png',
+                    title: 'People',
+                    lessonColor: Color(0xFF9c6644),
+                    stackColor1: Color(0xFF7f5539),
+                    stackColor2: Color(0xFFe6ccb2),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'People',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/greets.png',
+                    title: 'Home',
+                    lessonColor: Color(0xFF8d99ae),
+                    stackColor1: Color(0xFF2b2d42),
+                    stackColor2: Color(0xFFedf2f4),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Home',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/family.png',
+                    title: 'Places',
+                    lessonColor: Color(0xFF83c5be),
+                    stackColor1: Color(0xFF006d77),
+                    stackColor2: Color(0xFFb08968),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Places',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/the_body.png',
+                    title: 'Dining',
+                    lessonColor: Color(0xFFff8fab),
+                    stackColor1: Color(0xFFffb3c6),
+                    stackColor2: Color(0xFFffe5ec),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Dining',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/numbers.png',
+                    title: 'Travel',
+                    lessonColor: Color(0xFF48cae4),
+                    stackColor1: Color(0xFFade8f4),
+                    stackColor2: Color(0xFFcaf0f8),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Travel',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/food.png',
+                    title: 'Directions',
+                    lessonColor: Color(0xFFd5bdaf),
+                    stackColor1: Color(0xFFe3d5ca),
+                    stackColor2: Color(0xFFf5ebe0),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Directions',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/clothing.png',
+                    title: 'Health',
+                    lessonColor: Color(0xFFf6bd60),
+                    stackColor1: Color(0xFFf5cac3),
+                    stackColor2: Color(0xFFf7ede2),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Health',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/places.png',
+                    title: 'Emergency',
+                    lessonColor: Color(0xFFe9edc9),
+                    stackColor1: Color(0xFFfaedcd),
+                    stackColor2: Color(0xFFfefae0),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Emergency',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/the_body.png',
+                    title: 'Shopping',
+                    lessonColor: Color(0xFFff8fab),
+                    stackColor1: Color(0xFFffb3c6),
+                    stackColor2: Color(0xFFffe5ec),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Shopping',
+                  ),
+                ),
+                Flexible(
+                  child: LessonButton(
+                    image: 'assets/images/lessons/traffics.png',
+                    title: 'Money',
+                    lessonColor: Color(0xFFff9f1c),
+                    stackColor1: Color(0xFFffbf69),
+                    stackColor2: Color(0xFFffffff),
+                    nativeLanguage: 'English',
+                    learningLanguage: 'French',
+                    topic: 'Money',
+                  ),
+                ),
+              ],
+            ),            
+            SizedBox(height: 40),
           ],
         ),
       ),
