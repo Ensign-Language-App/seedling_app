780b87c75736f06b369f81e7e2fb9caa054c085e
Frank Lee <144109238+4anklee@users.noreply.github.com>
2024-07-14 22:02:30 -0600

    modify the theme colors, JULY 14, 2024

--- lib/utilities/theme.dart
+++ lib/utilities/theme.dart
@@ -4,9 +4,9 @@ ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.white,
     brightness: Brightness.light,
-    primary: const Color(0xFFC8D36A),
+    primary: const Color(0xFFF8B178),
     primaryContainer: Colors.grey[700],
-    secondary: const Color(0xFFD5E647),
+    secondary: const Color(0xFFF8B178),
   ),
 );
 
@@ -14,8 +14,8 @@ ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.fromSeed(
     seedColor: Colors.white,
     brightness: Brightness.dark,
-    primary: const Color(0xFFE0FA02),
+    primary: const Color(0xFFFFDFAF),
     primaryContainer: Colors.grey[700],
-    secondary: const Color(0xFFE0FA02),
+    secondary: const Color(0xFFE3B35B),
   ),
 );
